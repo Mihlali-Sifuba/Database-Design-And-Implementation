d97cfe7ee0aef2a9e0be108482e7a2eb06802abc
Sivuyile Sifuba <sivuyilesifuba@gmail.com>
2023-11-24 11:50:20 +0200

    Made modifications to the create statement so as to include unique keys to prevent duplication of data.

--- Global Superstore/CREATE TABLE countries_iso_codes.sql
+++ Global Superstore/CREATE TABLE countries_iso_codes.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `countries_iso_codes` (
   `Alpha-2 code` varchar(2) DEFAULT NULL,
   `Alpha-3 code` varchar(3) DEFAULT NULL,
   `Numeric code` int DEFAULT NULL,
-  `ISO 3166-2` varchar(45) DEFAULT NULL
+  `ISO 3166-2` varchar(45) DEFAULT NULL,
+  UNIQUE KEY `unq_country` (`Alpha-2 code`,`Alpha-3 code`,`Numeric code`,`ISO 3166-2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
