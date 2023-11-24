d485878e28b09cb270c3b1b199bdad8adaf15d0b
Sivuyile Sifuba <sivuyilesifuba@gmail.com>
2023-11-24 13:16:21 +0200

    I deleted the file with the old name.

--- Global Superstore/CREATE TABLE countries_iso_codes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `countries_iso_codes` (
-  `Country Name` varchar(255) DEFAULT NULL,
-  `Alpha-2 code` varchar(2) DEFAULT NULL,
-  `Alpha-3 code` varchar(3) DEFAULT NULL,
-  `Numeric code` int DEFAULT NULL,
-  `ISO 3166-2` varchar(45) DEFAULT NULL,
-  UNIQUE KEY `unq_country` (`Alpha-2 code`,`Alpha-3 code`,`Numeric code`,`ISO 3166-2`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
