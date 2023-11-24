9f2a1a19f5652b80a32d93530c6db93a33f4c5a4
Sivuyile Sifuba <sivuyilesifuba@gmail.com>
2023-11-24 13:41:44 +0200

    Removed columns from the customer table. We believe those columns refer to where the order was made and not where the customer lives.

--- Global Superstore/CREATE TABLE customer.sql
+++ Global Superstore/CREATE TABLE customer.sql
@@ -1,6 +1,5 @@
-CREATE TABLE IF NOT EXISTS `global superstore`.`customer` (
-  `customer_id` VARCHAR(255) NOT NULL,
-  `customer_name` VARCHAR(255) NULL,
-  `country_id` CHAR(3) NULL,
-  `city` VARCHAR(255) NULL,
-  PRIMARY KEY (`customer_id`));
+CREATE TABLE IF NOT EXISTS `customer` (
+  `customer_id` varchar(255) NOT NULL,
+  `customer_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
